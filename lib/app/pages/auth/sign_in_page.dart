5cf325362d2bf86e6978622e2c92e833e7f97287
Zakariyah Firmansyah <fzakariyah1@gmail.com>
2022-04-23 21:36:52 +0700

    add footer builder

--- lib/app/pages/auth/sign_in_page.dart
+++ lib/app/pages/auth/sign_in_page.dart
@@ -6,8 +6,20 @@ class SignInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SignInScreen(
-      providerConfigs: [EmailProviderConfiguration()],
+    return SignInScreen(
+      providerConfigs: const [EmailProviderConfiguration()],
+      footerBuilder: (context, _) {
+        return const Padding(
+          padding: EdgeInsets.only(top: 16),
+          child: Text(
+            'By signing in, you agree to our terms and conditions.',
+            style: TextStyle(color: Colors.grey),
+          ),
+        );
+      },
+      // footerBuilder: (context, _){
+      //   return const Padding(padding: EdgeInsets.only(top: 16),child: Text("By signing in, you agree to our terms and conditions.",style: TextStyle(color: Colors.grey),),)
+      // },
     );
   }
 }
